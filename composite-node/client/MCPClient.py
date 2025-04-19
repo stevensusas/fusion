fa3adcbb80febfe41c93deb4c6536b649a22c673
Steven Su <hanqisu@sas.upenn.edu>
2025-04-19 01:55:59 -0400

    fastmcp client

--- composite-node/client/MCPClient.py
+++ composite-node/client/MCPClient.py
@@ -4,7 +4,6 @@ from contextlib import AsyncExitStack
 
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
-
 from anthropic import Anthropic
 from dotenv import load_dotenv
 
